a6c919771f7eb0ee52dbbd778dee8830005b23cb
your-fav-bioinformatician <sara.salem@uoitc.edu.iq>
2026-05-02 18:58:20 +0300

    unnecessary

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -45,18 +45,18 @@ class Config:
     DEFAULT_LANG = "en"
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = None  # EG, paypal
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = False
-    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAACAgIAAx0CRMLKjgACBjtgHtmyflcZm_zsltAYGf_AtIy60AAClgEAAhZCawp7Vw4iU3POsB4E'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
-    CO_OWNER_ID = "" # a persons ID
+    # SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+    # SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    # WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    # DONATION_LINK = None  # EG, paypal
+    # CERT_PATH = None
+    # PORT = 5000
+    # DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    # STRICT_GBAN = False
+    # WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
+    # BAN_STICKER = 'CAACAgIAAx0CRMLKjgACBjtgHtmyflcZm_zsltAYGf_AtIy60AAClgEAAhZCawp7Vw4iU3POsB4E'  # banhammer marie sticker
+    # ALLOW_EXCL = False  # Allow ! commands as well as /
+    # CO_OWNER_ID = "" # a persons ID
 
 
 class Production(Config):
